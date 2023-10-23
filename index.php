154f2ba72899bf0f682fac227e4e4d734dd06147
U-JPR_WIN_DEV_ANT\Cgt_Jpr_PC_Admin <ganeshjatms@gmail.com>
2023-10-23 15:10:20 +0530

    dsd

--- index.php
+++ index.php
@@ -1,126 +1,51 @@
-<?php 
-require_once 'php_action/db_connect.php';
-
-session_start();
-
-if (isset($_SESSION['userId'])) {
-    header('location:' . $store_url . 'dashboard.php');		
-}
-
-$errors = array();
-
-// Check if the user is trying to log in via SSO
-if (isset($_SESSION['sso_login']) && $_SESSION['sso_login'] === true) {
-    header('Location: successful-sso-login.php');
-} elseif ($_POST) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    if (empty($username) || empty($password)) {
-        if (empty($username)) {
-            $errors[] = "Username is required";
-        } 
-
-        if (empty($password)) {
-            $errors[] = "Password is required";
-        }
-    } else {
-        $sql = "SELECT * FROM users WHERE username = '$username'";
-        $result = $connect->query($sql);
-
-        if ($result->num_rows == 1) {
-            $password = md5($password);
-            // exists
-            $mainSql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-            $mainResult = $connect->query($mainSql);
-
-            if ($mainResult->num_rows == 1) {
-                $value = $mainResult->fetch_assoc();
-                $user_id = $value['user_id'];
-
-                // set session
-                $_SESSION['userId'] = $user_id;
-
-                header('location:' . $store_url . 'dashboard.php');	
-            } else {
-                $errors[] = "Incorrect username/password combination";
-            } // /else
-        } else {		
-            $errors[] = "Username does not exist";		
-        } // /else
-    } // /else not empty username // password
-} // /if $_POST
-?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Stock Management System</title>
-    <!-- bootstrap -->
-    <link rel="stylesheet" href="assests/bootstrap/css/bootstrap.min.css">
-    <!-- bootstrap theme-->
-    <link rel="stylesheet" href="assests/bootstrap/css/bootstrap-theme.min.css">
-    <!-- font awesome -->
-    <link rel="stylesheet" href="assests/font-awesome/css/font-awesome.min.css">
-    <!-- custom css -->
-    <link rel="stylesheet" href="custom/css/custom.css">    
-    <!-- jquery -->
-    <script src="assests/jquery/jquery.min.js"></script>
-    <!-- jquery ui -->  
-    <link rel="stylesheet" href="assests/jquery-ui/jquery-ui.min.css">
-    <script src="assests/jquery-ui/jquery-ui.min.js"></script>
-    <!-- bootstrap js -->
-    <script src="assests/bootstrap/js/bootstrap.min.js"></script>
+    <title>PHP Login System</title>
 </head>
 <body>
-    <div class="container">
-        <div class="row vertical">
-            <div class="col-md-5 col-md-offset-4">
-                <div class="panel panel-info">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Please Sign in</h3>
-                    </div>
-                    <div class="panel-body">
-                        <div class="messages">
-                            <?php if ($errors) {
-                                foreach ($errors as $key => $value) {
-                                    echo '<div class="alert alert-warning" role="alert">
-                                    <i class="glyphicon glyphicon-exclamation-sign"></i>
-                                    ' . $value . '</div>';										
-                                    }
-                                } ?>
-                        </div>
-                        <form class="form-horizontal" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" id="loginForm">
-                            <fieldset>
-                              <div class="form-group">
-                                    <label for="username" class="col-sm-2 control-label">Username</label>
-                                    <div class="col-sm-10">
-                                      <input type="text" class="form-control" id="username" name="username" placeholder="Username" autocomplete="off" />
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="password" class="col-sm-2 control-label">Password</label>
-                                    <div class="col-sm-10">
-                                      <input type="password" class="form-control" id="password" name="password" placeholder="Password" autocomplete="off" />
-                                    </div>
-                                </div>                                
-                                <div class="form-group">
-                                    <div class="col-sm-offset-2 col-sm-10">
-                                      <button type="submit" class="btn btn-default"> <i class="glyphicon glyphicon-log-in"></i> Sign in</button>
-                                    </div>
-                                </div>
-                            </fieldset>
-                        </form>
-                        <!-- SSO Login Button -->
-                        <a href="sso-login.php">Login with SSO</a>
-                    </div>
-                    <!-- panel-body -->
-                </div>
-                <!-- /panel -->
-            </div>
-            <!-- /col-md-4 -->
-        </div>
-        <!-- /row -->
-    </div>
-    <!-- container -->    
+    <h1>PHP Login System</h1>
+
+    <?php
+session_start();
+
+if (isset($_SESSION['user_id'])) {
+    // User is already authenticated via the regular login system
+    echo "Welcome, " . $_SESSION['user_id'] . "!<br>";
+    echo '<a href="logout.php">Logout</a>';
+} elseif (isset($_SESSION['sso_login']) && $_SESSION['sso_login'] === true) {
+    // User has successfully logged in via SSO
+    header('Location: successful-sso-login.php');
+} else {
+    if (isset($_POST['login'])) {
+        // Handle regular login here
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        // Your authentication logic here
+        if ($validUser) {
+            $_SESSION['user_id'] = $username;
+            echo "Login successful. Welcome, " . $_SESSION['user_id'] . "!<br>";
+            echo '<a href="logout.php">Logout</a>';
+        } else {
+            echo "Invalid username or password. Please try again.<br>";
+        }
+    }
+}
+?>
+
+    <form method="POST" action="">
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username" required><br>
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" required><br>
+
+        <input type="submit" name="login" value="Login">
+    </form>
+
+    <br>
+
+    <!-- SSO Login Button (Sample) -->
+    <a href="sso-login.php">Login with SSO</a>
 </body>
 </html>
