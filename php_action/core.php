23db325b8a860df6f9ef8f5343a38007cbbe54e0
U-JPR_WIN_DEV_ANT\Cgt_Jpr_PC_Admin <ganeshjatms@gmail.com>
2023-10-24 17:10:29 +0530

    ssf

--- php_action/core.php
+++ php_action/core.php
@@ -6,8 +6,8 @@ require_once 'db_connect.php';
 
 // echo $_SESSION['userId'];
 
-if (isset($_SESSION['sso_login']) && $_SESSION['sso_login'] === true) {
-    header('Location: dashboard.php');
+if (isset($_SESSION['sso_login']) && $_SESSION['sso_login'] === true)
+	header('location:'. $store_url . 'dashboard.php');	
 
 if(!$_SESSION['userId']) {
 	header('location:'.$store_url);	
