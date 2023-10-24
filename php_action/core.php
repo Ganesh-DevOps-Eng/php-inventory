fb5afa99d1f7b3bb528c84363dda524853bda22d
Ganesh-DevOps-Eng <ganeshjatms@gmail.com>
2023-10-24 08:56:58 +0530

    sdsd

--- php_action/core.php
+++ php_action/core.php
@@ -6,12 +6,9 @@ require_once 'db_connect.php';
 
 // echo $_SESSION['userId'];
 
-if (!isset($_SESSION['userId']) || empty($_SESSION['userId'])) {
-    // Handle the case when 'userId' is not set
-    // You may want to provide an error message or use a different URL
-    header('location:' . $store_url);
-    exit; // Add an exit to stop script execution after redirection
-}
+if(!$_SESSION['userId']) {
+	header('location:'.$store_url);	
+} 
 
 
 
