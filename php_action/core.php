066763886bfaab83667e31a07d24a39ad6b11dd5
Ganesh-DevOps-Eng <ganeshjatms@gmail.com>
2023-10-24 19:38:56 +0530

    jhgjhgj

--- php_action/core.php
+++ php_action/core.php
@@ -6,12 +6,12 @@ require_once 'db_connect.php';
 
 // echo $_SESSION['userId'];
 
-// if (isset($_SESSION['sso_login']) && $_SESSION['sso_login'] === true){
-// 	header('location:'. $store_url . 'dashboard.php');
-// }
+if (isset($_SESSION['sso_login']) && $_SESSION['sso_login'] === true){
+	header('location:'. $store_url . 'dashboard.php');
+}
 		
 
-if(!$_SESSION['userId']) {
+elseif(!$_SESSION['userId']) {
 	header('location:'.$store_url);	
 } 
 
