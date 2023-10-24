6215625387ea7d7b88a5731cad022668f08b62be
U-JPR_WIN_DEV_ANT\Cgt_Jpr_PC_Admin <ganeshjatms@gmail.com>
2023-10-24 17:06:09 +0530

    ada

--- php_action/core.php
+++ php_action/core.php
@@ -6,10 +6,12 @@ require_once 'db_connect.php';
 
 // echo $_SESSION['userId'];
 
+if (isset($_SESSION['sso_login']) && $_SESSION['sso_login'] === true) {
+    header('Location: dashboard.php');
+
 if(!$_SESSION['userId']) {
 	header('location:'.$store_url);	
 } 
-
 
 
 ?>
